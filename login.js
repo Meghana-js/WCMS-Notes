9893bee63c4cdea8883096e8ddcd753910bf9be8
Meghana-js <meghanajshet@gmail.com>
2026-01-27 18:13:53 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,12 @@
+function login() {
+	var user=
+	document.getElementById("username").value;
+	var pass=document.getElementById("password").value;
+	
+	if(user=="wcms"&&pass=="wcms123") {
+		window.location.href="index.html";
+}else{
+	
+	document.getElementById("error").innerHTML="Invalid Username or Password";
+}
+}
